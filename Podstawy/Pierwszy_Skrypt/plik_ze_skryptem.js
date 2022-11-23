d24a4f3ebeeabc5a055b3d2647115b99bb7b8596
Krzysztof Mamcarz <110908983+KMamczas@users.noreply.github.com>
2022-11-23 01:59:59 +0100

    switch

--- Podstawy/Pierwszy_Skrypt/plik_ze_skryptem.js
+++ Podstawy/Pierwszy_Skrypt/plik_ze_skryptem.js
@@ -13,3 +13,33 @@ if (name) {
 } else {
     alert("Anulowałeś akcję, a okienko zwróciło " + name);
 }
+
+const animal = prompt("Wpisz jakiego masz zwierzaka");
+
+switch (animal) {
+    case "pies":
+        console.log("Psy są najlepsze");
+        break;
+    case "kot":
+        console.log("Koty są lepsze od psów");
+        break;
+    case "chomik":
+        console.log("Każdy chomik jest super");
+        break;
+    default:
+        console.log("Jakiś dziwny ten zwierzak");
+}
+
+const nr = 5;
+
+switch (true) {
+    case (nr <= 5):
+        console.log("Mało");
+    break;
+    case (nr > 5 && nr < 10):
+        console.log("Średnio");
+    break;
+    case (nr >= 10) :
+        console.log("Dużo");
+    break;
+}
