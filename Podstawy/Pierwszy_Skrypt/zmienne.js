d08816bebdfd72a183668c0dba64589fd11f9bb1
Krzysztof Mamcarz <110908983+KMamczas@users.noreply.github.com>
2022-11-23 01:37:19 +0100

    zmienne

---
+++ Podstawy/Pierwszy_Skrypt/zmienne.js
@@ -0,0 +1,15 @@
+// var txt = "Ala ma kota";
+// var nr = 20;
+// var url = "kontakt.html";
+
+// console.log(txt);
+// console.log(nr);
+// console.log(url);
+
+let txt = "Przykładowy tekst"; //zmienna
+txt = "Inny tekst"; //zmieniamy wartość
+
+const nr = 102; //stała
+nr = 103; //błąd - nie można przypisać nowej wartości
+
+
