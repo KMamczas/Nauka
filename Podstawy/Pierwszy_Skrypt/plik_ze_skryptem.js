dbe01f59bc73c09e366c1af05622861a8c455bbc
Krzysztof Mamcarz <110908983+KMamczas@users.noreply.github.com>
2022-11-23 01:32:49 +0100

    alert confirm prompt

--- Podstawy/Pierwszy_Skrypt/plik_ze_skryptem.js
+++ Podstawy/Pierwszy_Skrypt/plik_ze_skryptem.js
@@ -1,1 +1,15 @@
 console.log("Hello Word!")
+
+if (confirm("Czy jesteś pewien, że chcesz kontynuować?")) {
+    alert("No to kontynuuj...");
+} else {
+    alert("Przykro mi, że nie chcesz kontynuować...");
+}
+
+const name = prompt("Podaj swoje imię:");
+
+if (name) {
+    alert("Witaj " + name);
+} else {
+    alert("Anulowałeś akcję, a okienko zwróciło " + name);
+}
