00cf7de55a40d7dae6268e89535216feaaa868cd
Krzysztof Mamcarz <110908983+KMamczas@users.noreply.github.com>
2022-12-02 00:32:00 +0100

    string

---
+++ Podstawy/string.js
@@ -0,0 +1,13 @@
+const age = 10;
+
+const text = "Ten pies ma " + age + " lat";
+const text1 = 'Ten kot ma ' + age + ' lat';
+const text2 = `Ten chomik ma ${age} lat`;
+const a = 112;
+const b = 120;
+
+const text3 = "Cena produktu A to " + a + "zł, cena produktu B to " + b + "zł, a suma to " + (a+b)+ "zł";
+const text4 = `Cena produktu A to ${a}zł, cena produktu B to ${b}zł, a suma to ${a+b}zł`; //interpolacja  chyba
+
+console.log(text3)
+console.log(text4)
