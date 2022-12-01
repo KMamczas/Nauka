7abac8b529f26008b9145777a012b60fb894bf10
Krzysztof Mamcarz <110908983+KMamczas@users.noreply.github.com>
2022-12-02 00:14:56 +0100

    Update typyDanych.js

--- Podstawy/typyDanych.js
+++ Podstawy/typyDanych.js
@@ -27,3 +27,26 @@ console.log(txt1A === txt1B); //true
 const txt2A = new String("Ala");
 const txt2B = new String("Ala");
 console.log(txt2A === txt2B); //false
+
+//number to string
+
+const nr2 = 102;
+
+console.log("" + nr2); //"102"
+console.log(nr2.toString()); //"102"
+console.log(String(nr2)); //"102"
+
+//string to number
+
+console.log( Number("100")); //100
+console.log( Number("50.5")); //50.5
+console.log( Number("50px")); //NaN
+
+console.log( parseInt("24px", 10)); //24
+console.log( parseInt("26.5", 10)); //26
+console.log( parseInt("100kot", 10)); //100
+console.log( parseInt("Ala102", 10)); //NaN - zaczyna się od liter
+console.log( parseInt("Hello", 10)); //NaN
+
+console.log( "20px" + "20px"); //20px20px
+console.log( parseInt("20px", 10) + parseInt("20px", 10) + "px"); //40px
