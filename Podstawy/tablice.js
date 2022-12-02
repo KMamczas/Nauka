4cd02cc6f0f022c7f506a1ce51d15a3fab16295f
Krzysztof Mamcarz <110908983+KMamczas@users.noreply.github.com>
2022-12-03 00:18:17 +0100

    Koniec tablic/początek funkcji

--- Podstawy/tablice.js
+++ Podstawy/tablice.js
@@ -42,3 +42,68 @@ console.log(tab2); //["Ala", "ma", "kota"];
 const txt5 = "Ala ma kota";
 const tab4 = txt5.split("");
 console.log(tab4); 
+
+
+
+
+const tab7 = ["Marcin", "Ania", "Agnieszka"];
+
+for (const el of tab7) { //el to nazwa zmiennej wymyślona przez nas
+    console.log(el); //"Marcin", "Ania"...
+}
+
+for (const xxx of tab7) { //xxx to nazwa zmiennej wymyślona przez nas
+    console.log(xxx.toUpperCase()); //"MARCIN"...
+}
+
+for (let lorem of tab7) { //niektórzy używają tutaj let zamiast const
+    console.log(lorem);
+}
+
+
+
+
+
+const tabw = [
+    ["a1", "a2", "a3", "a4", "a5", "a6"],
+    ["b1", "b2", "b3", "b4", "b5", "b6"],
+    ["c1", "c2", "c3", "c4", "c5", "c6"],
+]
+
+console.log(tabw[0]); //["a1", "a2", "a3", "a4", "a5", "a6"]
+console.log(tabw[0].length); //6
+console.log(tabw[0][1]); //"a2"
+console.log(tabw[2][3]); //"c4"
+
+
+const level = [
+    [1, 1, 0, 0, 2, 2, 0, 0, 1, 1],
+    [1, 0, 0, 0, 2, 2, 0, 0, 0, 1],
+    [1, 0, 1, 1, 2, 2, 1, 1, 0, 1],
+    [1, 0, 2, 2, 2, 2, 2, 2, 0, 1],
+    [2, 2, 2, 2, 1, 1, 2, 2, 2, 2],
+    [2, 2, 2, 2, 1, 1, 2, 2, 2, 2],
+    [1, 0, 2, 2, 2, 2, 2, 2, 0, 1],
+    [1, 0, 1, 1, 2, 2, 1, 1, 0, 1],
+    [1, 0, 0, 0, 2, 2, 0, 0, 0, 1],
+    [1, 1, 0, 0, 2, 2, 0, 0, 1, 1]
+];
+
+
+let str = "";
+
+for (const subTab of level) {
+    //pod subTab mamy każdą kolejną podtablicę
+
+    for (const el of subTab) {
+        switch (el) {
+            case 0 : str += "🟩"; break;
+            case 1 : str += "🟫"; break;
+            case 2 : str += "⬛"; break;
+        }
+    }
+
+    str += "\n";
+}
+
+console.log(str);
