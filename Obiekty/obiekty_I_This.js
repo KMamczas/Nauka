5fe5d8ee86fd69a23b0138afe9babd6681e5a04f
Krzysztof Mamcarz <110908983+KMamczas@users.noreply.github.com>
2022-12-06 00:06:19 +0100

    Update obiekty_I_This.js

--- Obiekty/obiekty_I_This.js
+++ Obiekty/obiekty_I_This.js
@@ -56,3 +56,52 @@ dog2.showText(); //"Lubię walczyć ze złem"
 dog2["name"]; //"Szama"
 dog2["speed"]; //1000
 dog2["showText"](); //"Lubię walczyć ze złem"
+
+
+
+
+
+const person = {
+    name: "Marcin",
+
+    pet: {
+        name: "Szama",
+        color: "brown",
+        speed: 1000,
+
+        collar: {
+            color: "red",
+            length: "25cm"
+        },
+
+        favoriteFood: ["mięso", "mięso", "mięso"]
+    }
+}
+
+person.name //"Marcin"
+person.pet.name //"Szama"
+person.pet.collar.color //"red"
+person.pet.favoriteFood[1] //"mięso"
+
+
+
+
+const cat = {
+    name: "Szama",
+    speed: 1000,
+    showText() {
+        return "Lubię walczyć ze złem";
+    }
+}
+
+cat.type = "kot";
+cat.legs = 4;
+cat.eat = function() {
+    return "Jem dobre rzeczy";
+}
+
+console.log(cat.eat());
+console.log(cat.showText());
+
+
+
